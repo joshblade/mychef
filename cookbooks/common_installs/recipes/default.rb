d01b64a4d0bc70315d66c2fb3960bdb80827f634
joshblade <robertmburke65@gmail.com>
2017-05-24 22:53:15 -0400

    Added some cookbooks

---
+++ cookbooks/common_installs/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: common_installs
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+windows_package '7zip' do
+  source 'https://reidevopsstorage.file.core.windows.net/ise/prerequisites/7z920-x64.msi?sv=2016-05-31&ss=bfqt&srt=sco&sp=rwdlacup&se=2017-07-25T06:49:34Z&st=2017-05-24T22:49:34Z&spr=https&sig=8OBSnS7SmSZz2z4UlDXAoGrMqwVMQMN%2FjlwdHARz4BQ%3D'
+end
