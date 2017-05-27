e8f47bb9b6d5c93f003c2d2decf78b7ec53c37d4
robert.burke <robert.burke@reisystems.com>
2017-05-26 23:24:15 -0400

    added some psscropts for a different project

---
+++ Utilization-tool/DailyLoadSignincsRefer.bat
@@ -0,0 +1,4 @@
+for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
+set todaysdate=%Year%-0%Month%-%Day%
+
+"C:\Program Files (x86)\Log Parser 2.2\logparser.exe" "SELECT * INTO Stg_IISLogs FROM \\hrsafs.reisys.com\adminftp\IISLOGS\IISLOGS_Internal\*.log WHERE cs(Referer) like '%%EAuthNS/Account/SignIn%%' and to_string(to_date(date),'yyyy-MM-dd') ='%ToDaysDate%'"  -i:iisw3c -o:SQL -recurse -server:hsa-db-admin -database:IntEnvAnalysis -username:sa -password:#GemsUser1 -createTable: ON  
---
+++ Utilization-tool/bulkload_Signin.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Log Parser 2.2\logparser.exe" "SELECT * INTO Stg_IISLogs FROM \\hrsafs.reisys.com\adminftp\IISLOGS\IISLOGS_Internal\*.log WHERE cs-uri-stem like '%%EAuthNS/Account/SignIn%%' or cs(Referer) like '%%EAuthNS/Account/SignIn%%'"  -i:iisw3c -o:SQL -recurse -server:hsa-db-admin -database:IntEnvAnalysis -username:sa -password:#GemsUser1 -createTable: ON  
